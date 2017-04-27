9ff99d0010542f6b918f83c634986ed2b3fd20b3
Kol <ask_kol@yahoo.co.uk>
2017-04-27 16:06:01 +0100

    Setting up layout of website

--- LetsDoReactJs/LetsDoReactJs/Startup.cs
+++ LetsDoReactJs/LetsDoReactJs/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using React.AspNet;
 
 namespace LetsDoReactJs
 {
@@ -16,22 +17,23 @@ namespace LetsDoReactJs
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddReact();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole();
 
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseReact(Configure => Configure.SetLoadReact(true));
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+
+
         }
     }
 }
