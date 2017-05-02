eea891a5e022e5bf26866cb25d3f8d10a9ee4072
Kol <ask_kol@yahoo.co.uk>
2017-05-02 16:22:20 +0100

    Added project

---
+++ LetsDoReactJs/PizzaBotManager/Models/BaseModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaBotManager.Models
+{
+    public class BaseModel
+    {
+        public string ApplicationName { get; private set; }
+        public string PageTitle { get; set; }
+        public BaseModel(string applicationName="")
+        {
+            this.ApplicationName = !String.IsNullOrEmpty(applicationName) ? applicationName : "PizzaBM";
+        }
+    }
+}
