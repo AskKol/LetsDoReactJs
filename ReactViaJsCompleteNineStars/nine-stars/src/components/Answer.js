aa6392fc368ef39f4126cd5ec105993f52000cfe
kol <ask_kol@yahoo.co.uk>
2018-01-02 16:09:39 +0000

    Star nine game

---
+++ ReactViaJsCompleteNineStars/nine-stars/src/components/Answer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Answer = (props) =>
+{
+    return (
+        <div  className="col-sm-5">
+            ...
+        </div>
+        );
+};
+
+export default Answer;
---
+++ ReactViaJsCompleteNineStars/nine-stars/src/components/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Button = (props) =>
+{
+    return (
+        <div  className="col-sm-2">
+            <button>=</button>
+        </div>
+        );
+};
+
+export default Button;
